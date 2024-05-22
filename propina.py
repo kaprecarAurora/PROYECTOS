e0675031c4db8c06716de63f7b68173fcfd9c6a0
kaprecarAurora <kaprecarhack@gmail.com>
2024-05-22 17:25:31 +0200

    Añadiendo bucles

--- propina.py
+++ propina.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
 
-monto_cuenta = float(input("Introduzca la cantidad total de la cuenta:  "))
-calidad_servicio = input("¿Como ha sido la calidad del servicio?: ")
+def calidad_servicio_calcular():
+    propinas = {"excelente": 20, "bueno": 15, "regular": 10, "malo": 5}
+    while True:
+        monto_cuenta = float(input("Introduzca la cantidad total de la cuenta:  "))
+        calidad_servicio = input("¿Como ha sido la calidad del servicio?: ")
 
-def calidad_servicio_calcular(monto_cuenta, calidad_servicio): # Funcion que calcula la propina segun la calidad del servicio
+        porcentaje = propinas.get(calidad_servicio)
 
-    propinas = {"excelente": 20, "bueno": 15, "regular": 10, "malo": 5} # Diccionario con los porcentajes de propina segun la calidad del servicio
-    porcentage = propinas.get(calidad_servicio) # Obtenemos el porcentaje de propina segun la calidad del servicio
-
-    if porcentage: #Valoramos si la calidad del servicio es reconocida
-        return (monto_cuenta * porcentage) / 100
-    else:
-        return "Calidad del servicio no reconocida"
-
-print(calidad_servicio_calcular(monto_cuenta, calidad_servicio))
-
+        if porcentaje:
+            return (monto_cuenta * porcentaje) / 100
+            
+        else:
+            print("Calidad del servicio no reconocida")
+            continue
+               
+while True:
+    print(calidad_servicio_calcular())
